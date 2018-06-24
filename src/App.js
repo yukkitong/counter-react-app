a2077263871cb3a50bbb824c1f3a5dfc42b88f46
육기통 <yukkitong@yuggitong-ui-MacBook-Pro.local>
2018-06-24 14:42:07 +0900

    react redux basic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
 
-
-
-
-
-
 import React, { Component } from 'react';
+import { createStore } from 'redux';
 import './App.css';
 import { Button } from 'react-bootstrap';
 
+// reducer
+const counterReducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREASE':
+      return { count: state.count + 1 };
+    case 'DECREASE':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
+// store
+const store = createStore(counterReducer);
+
+const increase = () => ({ type: 'INCREASE' });
+const decrease = () => ({ type: 'DECREASE' });
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = store.getState(); // initial state
+  }
+
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => {
+      this.setState(store.getState());
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe && this.unsubscribe();
+  }
+
+  // event handler for increament
+  increase = () => {
+    store.dispatch(increase());
+  }
+
+  // event handler for decreament
+  decrease = () => {
+    store.dispatch({ type: 'DECREASE' });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1></h1>
-        <Button />
-        <Button />
+        <h1>{this.state.count}</h1>
+        <Button bsStyle="primary" onClick={this.increase}>INCREASE</Button>
+        <Button bsStyle="default" onClick={this.decrease}>DECREASE</Button>
       </div>
     );
   }
